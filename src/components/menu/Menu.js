732b4b3f6b24c0b24c59b128ef61709cf1f2afa0
fahad <fjkiani@icloud.com>
2021-02-04 14:35:59 -0500

    commit

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -22,7 +22,7 @@ export default function Menu({data}) {
                 <div className="item-info">
                 <header>  
                 <h4>{title}</h4>
-                <h4 className="price ">{price}</h4>  
+                <h4 className="price">{price}</h4>  
                 </header>
                 <p className="item-text">{description}</p>
 
